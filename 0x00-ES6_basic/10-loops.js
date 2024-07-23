4e65c33d7b41aa4f04ae971e54549c59966ae433
Lamba237 <simontanyi2004@gmail.com>
2024-07-23 07:12:36 +0000

    updates

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let index of array) {
-    // code goes here
+  let index = 0; // use to keep track of the current index
+  for (let value of array) {
+    array[index] = appendString + value;
+    index++;
   }
 
   return array;
