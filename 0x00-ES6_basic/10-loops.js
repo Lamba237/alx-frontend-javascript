810c0cec5e565d91d28986c99d68cd813e0f7718
Lamba237 <simontanyi2004@gmail.com>
2024-07-23 07:02:49 +0000

    updates

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let [index, value] of array.entries()) {
+	  //array.entries() gets both the index and value
     array[index] = appendString + value;
   }
 
