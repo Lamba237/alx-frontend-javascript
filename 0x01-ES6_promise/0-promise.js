614db7576082dc9fad7243dc218592c449a50800
Lamba237 <simontanyi2004@gmail.com>
2024-07-24 23:21:35 +0000

    updates

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,13 @@
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const success = true;
+  
+        if (success) {
+          resolve("API call succeeded!");
+        } else {
+          reject("API call failed.");
+        }
+      }, 1000); // Simulate a 1-second delay
+    });
+  }
