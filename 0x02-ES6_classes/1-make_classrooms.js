5bae07eef1894619b9ec2e352c1e5c4f459ac1bd
Lamba237 <simontanyi2004@gmail.com>
2024-07-25 22:19:01 +0000

    import a class from another and implemented a function that returns an array of 3 classRoom objects

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import classRoom from './0-classroom';
 
-function initializeRooms() {
+export default function initializeRooms() {
   const room1 = new classRoom(19);
   const room2 = new classRoom(20);
   const room3 = new classRoom(21);
