d76f11060376d4a7f0e0c9ced239fcf2e9c982a3
Lamba237 <simontanyi2004@gmail.com>
2024-07-25 00:47:29 +0000

    updates

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({ status: 200, body: 'success' });
     } else {
-      reject('The fake API is not working currently');
+      reject(Error('The fake API is not working currently'));
     }
   });
 }
