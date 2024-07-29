5e317b8e7860687495b3d2560da8b613bcf413b4
Lamba237 <simontanyi2004@gmail.com>
2024-07-29 07:14:35 +0000

    updates

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,6 +5,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
   const promises = [promise1, promise2];
+  const result = [];
 
   return Promise.allSettled(promises).then((results) => results.map((result) => ({
     status: result.status,
