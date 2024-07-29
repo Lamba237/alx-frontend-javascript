6533b42824c0515b641c2546a6bba06e6348ab17
Lamba237 <simontanyi2004@gmail.com>
2024-07-29 19:36:15 +0000

    A function that throws an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,11 +1,13 @@
 export default function guardrail(mathFunction) {
   const queue = [];
+  let result;
   try {
-    const result = mathFunction();
-    queue.push(result);
+    result = mathFunction();
   } catch (error) {
-    queue.push(error.message);
+    result = `Error: ${error.message}`;
   }
+  queue.push(result);
   queue.push('Guardrail was processed');
+
   return queue;
 }
