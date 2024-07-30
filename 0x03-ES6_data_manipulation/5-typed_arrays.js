c63a029de39884fb5b5a95702fdc2d40450cb486
Lamba237 <simontanyi2004@gmail.com>
2024-07-30 22:11:37 +0000

    type-like function

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,13 +2,14 @@ export default function createInt8TypedArray(length, position, value) {
   if (typeof length !== 'number' || typeof position !== 'number' || typeof value !== 'number') {
     throw new Error();
   }
-  // Position checks
-  if (position >= length || position < 0) {
-    throw new Error('Position outside range');
-  }
-  const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
-  int8View[position] = value;
 
-  return buffer;
+  const typeArray = new ArrayBuffer(length);
+  const val = new DataView(typeArray);
+
+  try {
+    val.setInt8(position, value);
+  } catch (e) {
+    throw Error('Position outside range');
+  }
+  return val;
 }
