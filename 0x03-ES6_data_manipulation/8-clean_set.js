fe75852811d1842d1da37dcf25f0bec88c1539fc
Lamba237 <simontanyi2004@gmail.com>
2024-07-31 00:08:22 +0000

    updates

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,9 @@
 export default function cleanSet(newSet, startString) {
   const result = [];
+
+  if (typeof newSet !== 'object' || typeof startString !== 'string') {
+    return '';
+  }
   // 1) First iterate over our Set
   for (const value of newSet) {
     // 2) check if a string starts with startString variable
