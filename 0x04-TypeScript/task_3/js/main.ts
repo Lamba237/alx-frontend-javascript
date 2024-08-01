df8081569e7f658b432e846f9e730cbd9cb908fc
Lamba237 <simontanyi2004@gmail.com>
2024-08-01 06:36:52 +0000

    updates for task 3

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,20 @@
+/// <reference path="./task_3/js/crud.d.ts" />
+
+import { insertRow } from "./crud";
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./task_3/js/CRUD";
+
+const row: RowElement = {
+    firstName: "Guillaume",
+    lastName: "Salva"
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = {
+    ...row,
+    age: 23
+};
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
