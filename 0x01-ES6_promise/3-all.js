d65e329d18ab271e3b06fa35f53b2c1ac82e9d74
root <simontanyi2004@gmail.com>
2024-10-02 20:09:40 +0000

    Updates for task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,6 +7,6 @@ export default function handleProfileSignup() {
     const [photo, user] = results;
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   }).catch(() => {
-    console.log('Signup system offline');
+    console.error('Signup system offline');
   });
 }
