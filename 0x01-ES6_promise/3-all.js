49fb640a4c478f961791589426b8387442ceaf1d
root <simontanyi2004@gmail.com>
2024-10-02 19:39:54 +0000

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  // Using promise.all to handle all promises returned by
+  // UploadPhoto and CreateUser
+  Promise.all([uploadPhoto(), createUser()]).then((results) => {
+    const [photo, user] = results;
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
+}
