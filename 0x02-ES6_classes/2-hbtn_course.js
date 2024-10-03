883415081c5fadaf59625bafdbc1efd23def8cff
root <simontanyi2004@gmail.com>
2024-10-03 16:30:13 +0000

    A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // getter and setter for length attribute
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  // getter and setter for students attribute
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (typeof newStudents !== 'string') {
+      throw new TypeError('Student must be an array of Strings');
+    }
+    this._students = newStudents;
+  }
+}
