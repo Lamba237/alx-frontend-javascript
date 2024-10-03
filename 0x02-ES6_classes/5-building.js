e261c192e20e29fc002af5bb7e017e726049498c
root <simontanyi2004@gmail.com>
2024-10-03 21:24:40 +0000

    updates

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,8 @@
 export default class Building {
   constructor(sqft) {
+    if (new.target === Building) {
+      throw new Error('Building is an abstract class and cannot be instantiated directly.');
+    }
     this._sqft = sqft;
   }
 
