e83e16ecb3da47190a63a8bfd92402f54619bd36
root <simontanyi2004@gmail.com>
2024-10-03 18:45:54 +0000

    updates

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,21 +1,15 @@
 class Building {
   constructor(sqft) {
+    if (new.target === Building) {
+      throw new Error('Building is an abstract class and cannot be instantiated directly.');
+    }
     this._sqft = sqft;
   }
 
-  // Implementing a getter and setter for our sqft attribute
   get sqft() {
     return this._sqft;
   }
 
-  set sqft(newSqft) {
-    if (typeof newSqft !== 'number') {
-      throw new TypeError('square footage must be a number');
-    }
-    this._sqft = newSqft;
-  }
-
-  // An abstract method
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
