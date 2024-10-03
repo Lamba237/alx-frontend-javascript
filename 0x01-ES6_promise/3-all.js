362cc3736091f6c812c106978d63595004a81787
root <simontanyi2004@gmail.com>
2024-10-03 05:45:30 +0000

    returned Promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   // Using promise.all to handle all promises returned by
   // UploadPhoto and CreateUser
-  Promise.all([uploadPhoto(), createUser()]).then((results) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((results) => {
     const [photo, user] = results;
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   }).catch(() => {
