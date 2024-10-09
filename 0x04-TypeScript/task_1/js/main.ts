70c2263f5d8a1df7d651e740ea8a8f21fecc3b65
Lamba237 <simontanyi2004@gmail.com>
2024-10-10 00:58:52 +0100

    Added a function printTeacher that returns the first of firstName and the full lastName

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,20 @@ interface Teacher {
   [key: string]: any
 }
 
+// interface Director inherits attributes from Teacher
 interface Directors extends Teacher {
   numberOfReports: number
 }
+
+// function printTeacher
+function printTeacher(firstName: string, lastName: string) {
+  return `${firstName.charAt(0)} ${lastName}`;   
+}
+
+// interface for function named printTeacherFunction
+
+interface printTeacherFunction {
+    firstName: string,
+    lastName: string
+}
+
