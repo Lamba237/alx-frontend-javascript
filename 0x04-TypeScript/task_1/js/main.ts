38ee41f48f304cd4a427b12f3f96c337fa10ee38
Lamba237 <simontanyi2004@gmail.com>
2024-10-10 01:35:55 +0100

    Student class created defined an interface for the constructor of the class define an interface for the class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -23,4 +23,33 @@ interface printTeacherFunction {
     firstName: string,
     lastName: string
 }
+// Implement an interface for constructor
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
 
+// define an interface for the class
+interface StudentClassInterface {
+    firstName: string,
+    lastName: string,
+    workOnHomework(): string,
+    displayName(): string
+}
+
+// Implement the studentClass
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string , lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return `Currently working`;
+  }
+
+  displayName(): string {
+    return `${this.firstName}`;
+  }
+}
