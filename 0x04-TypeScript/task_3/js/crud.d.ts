1a04c2c09f0fb863b2d250e4c177a67b3bdf6cc3
Lamba237 <simontanyi2004@gmail.com>
2024-10-11 09:33:00 +0100

    Ambient Namespaces

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,13 @@
+import { RowID, RowElement } from"./interface";
+
+export function insertRow(row: RowElement): number {
+    throw new Error("Function not implemented.");
+}
+export function updateRow(arg0: number, arg1: number): RowElement {
+    throw new Error("Function not implemented.");
+}
+
+export function deleteRow(newRowID: number) {
+    throw new Error("Function not implemented.");
+}
+
