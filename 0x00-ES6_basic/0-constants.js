3b03dcfd107efed292503447791f562be5df0ee9
Lamba237 <simontanyi2004@gmail.com>
2025-07-04 12:41:31 +0100

    Project Dependency added

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+export default function taskFirst() {
+	const task = 'I prefer const when I can.';
+	return task;
+}
+
+export default function getLast() {
+	return ' is okay';
+}
+
+export default function taskNext() {
+	let combination = 'But sometimes let';
+	combination += getLast();
+
+	return combination;
+}
